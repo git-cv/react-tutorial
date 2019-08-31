f0f911ddb96b25bd4411a82771a4a8fc69d40741
Greg Herbowicz <herbowicz@gmail.com>
2019-08-31 20:54:24 +0200

    app use

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,4 @@
-//import dependencies
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
@@ -7,11 +7,12 @@ const morgan = require('morgan');
 const jwt = require('express-jwt');
 const jwksRsa = require('jwks-rsa');
 
-const serverless = require('serverless-http');
-const router = express.Router();
 
 // define the Express app
 const app = express();
+
+const serverless = require('serverless-http');
+const router = express.Router();
 
 // the database
 const questions = [];
@@ -29,18 +30,18 @@ app.use(cors());
 // log HTTP requests
 app.use(morgan('combined'));
 
-// retrieve all questions
-// app.get('/', (req, res) => {
-//   const qs = questions.map(q => ({
-//     id: q.id,
-//     title: q.title,
-//     description: q.description,
-//     answers: q.answers.length,
-//   }));
-//   res.send(qs);
-// });
+retrieve all questions
+app.get('/', (req, res) => {
+  const qs = questions.map(q => ({
+    id: q.id,
+    title: q.title,
+    description: q.description,
+    answers: q.answers.length,
+  }));
+  res.send(qs);
+});
 
-app.get('/.netlify/functions/server', router);  // route to lambda
+app.use('/.netlify/functions/server', router);  // route to lambda
 app.handler = serverless(app);
 
 // get a specific question
